8c2a96640c8e18ff9b764121cca623b541760d5a
hohangyau <hohangyau@gmail.com>
2022-06-17 16:27:13 +0800

    connect to db

--- app.js
+++ app.js
@@ -1,49 +1,65 @@
 const express = require('express');
 const app = express();
-
+var mysql = require('mysql');
 const port = 3000;
 
-const students = {
-    223445:{
-        id:223445,
-        firstName: "Mike",
-        lastName: "Wong",
-        class:"3A",
-        nationality: "Singapore"
-    },
-    223446: {
-        id:223447,
-        firstName: "Edwin",
-        lastName: "Cheung",
-        class:"3A",
-        nationality: "Hong Kong"
-    },
-    223447: {
-        id:223447,
-        firstName: "Harry",
-        lastName: "Ho",
-        class:"3A",
-        nationality: "Hong Kong"
-    }
-}
+var con = mysql.createConnection({
+    host: "mydb-awssql.cy4oinulfb9u.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "12345678",
+    database: "mydb"
+  });
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    //var sql = "CREATE TABLE students (id INT NOT NULL AUTO_INCREMENT, firstName VARCHAR(255), lastName VARCHAR(255), class VARCHAR(15), nationality VARCHAR(255), PRIMARY KEY (id))";
+  //  var sql = "INSERT INTO students (id, firstName, lastName, class, nationality) VALUES (223445,'Mike', 'Wong', '3A', 'Singapore')" ;
+  });
+
+
+  const executeSQL = async (sql)=> {
+    return new Promise(data => {
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            try {
+                data(result);
+
+            } catch (error) {
+                data({});
+                throw error;
+            }
+        });
+    })
+
+
+  }
 
 app.get('/', (req, res) => {
     res.send('hello rest api')
 })
 
-app.get('/fetchStudents', (req, res) => {
+app.get('/fetchStudents', async (req, res) => {
     //console.log(req.query);
     const id = req.query.id;
+    const cla = req.query.class;
     if(typeof id !== 'undefined'){
-        const student = students[parseInt(id)]
-        console.log(students[223445]);
-        console.log(students.id);
-        res.send(student)
+        let sql = `SELECT * FROM students where id = ${id}`
+        let data = await executeSQL(sql);
+        console.log(data)
+        res.send(data)
     }
 
-  
+    if(typeof cla !== 'undefined'){
+        let sql = `SELECT * FROM students where class = "${cla}"`
+        console.log(sql)
+        let data = await executeSQL(sql);
+        res.send(data)
+    }
+
+
 })
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
 });
+
